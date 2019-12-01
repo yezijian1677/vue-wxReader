3b17cd0ac21ce96555af7dd18fa353fc0dc13901
yezijian <1677390657@qq.com>
2019-12-01 14:37:08 +0800

    loading animation

--- src/components/ebook/EbookSlide.vue
+++ src/components/ebook/EbookSlide.vue
@@ -3,7 +3,7 @@
         <div class="slide-content-wrapper" v-show="menuVisible && settingVisible === 3">
             <transition name="fade-slide-right">
                 <div class="content" v-if="settingVisible === 3">
-                    <div class="content-page-wrapper">
+                    <div class="content-page-wrapper" v-if="bookAvailable">
                         <div class="content-page">
                           <component :is="currentTab === 1 ? content : bookmark"/>
                         </div>
@@ -11,6 +11,9 @@
                           <div class="content-page-tab-item" :class="{'selected': currentTab === 1}" @click="selectTab(1)">{{$t('book.navigation')}}</div>
                           <div class="content-page-tab-item" :class="{'selected': currentTab === 2}" @click="selectTab(2)">{{$t('book.bookmark')}}</div>
                         </div>
+                    </div>
+                    <div class="content-empty" v-else>
+                        <ebook-loading/>
                     </div>
                 </div>
             </transition>
@@ -22,10 +25,15 @@
 <script>
     import {ebookMinx} from '../../utils/mixin'
     import EbookSlideContents from  './EbookSlideContents'
+    import EbookLoading from  './EbookLoading'
 
     export default {
         name: "EbookSlide",
+        components: {EbookLoading},
         mixins: [ebookMinx],
+        comments:{
+            EbookLoading
+        },
         data(){
           return {
             currentTab: 1,
