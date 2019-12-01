c5ac05d31a21d710f0a7836167f13f85fd772cb6
yezijian <1677390657@qq.com>
2019-12-01 19:08:53 +0800

    tag 2

--- src/components/ebook/EbookBookmark.vue
+++ src/components/ebook/EbookBookmark.vue
@@ -6,7 +6,7 @@
             </div>
             <div class="ebook-bookmark-text">{{text}}</div>
         </div>
-        <div class="ebook-bookmark-icon-wrapper">
+        <div class="ebook-bookmark-icon-wrapper":style="isFixed ? fixedStyle : {}">
             <div class="icon"></div>
             <bookmark :color="color" :width="15" :height="35"/>
         </div>
@@ -33,19 +33,25 @@
             },
             threshold() {
                 return realPx(55);
+            },
+            fixedStyle() {
+                return{
+                    position: 'fixed',
+                    top: 0,
+                    right: `${(window, innerWidth - this.$refs.bookmark.clientWidth) / 2}px`
+                }
             }
         },
         watch: {
             offsetY(v){
                 if (v >= this.height && v <= this.threshold) {
-                    this.$refs.bookmark.style.top = `${-v}px`;
-                    this.text = this.$t('book.pulldownAddMark');
-                    this.color = WHITE;
+                    this.beforeThreshold(v);
                 }else if (v >= this.threshold) {
-                    //超越临界状态
-                    this.$refs.bookmark.style.top = `${-v}px`;
-                    this.text = this.$t('book.releaseAddMark');
-                    this.color = BLUE;
+                    this.afterThreshold(v);
+                }else if (v > 0 && v < this.height) {
+
+                }else if (v === 0) {
+                    this.restore();
                 }
             }
         },
@@ -53,6 +59,59 @@
             return{
                 text: '',
                 color: WHITE,
+                isFixed: false
+            }
+        },
+        methods: {
+            restore() {
+                setTimeout(() => {
+                    this.$refs.bookmark.style.top = `${-this.height}px`;
+                    this.$refs.iconDown.style.transform = 'rotate(0deg)';
+                }, 200);
+                if (this.isFixed) {
+                    this.setIsBookmark(true);
+                }else {
+                    this.setIsBookmark(false);
+                }
+            },
+            beforeHeight() {
+                if (this.isBookmark) {
+                    this.text = this.$t('book.pulldownDeleteMark');
+                    this.color = BLUE;
+                    this.isFixed = true;
+                } else {
+                    this.text = this.$t('book.pulldownAddMark');
+                    this.color = WHITE;
+                    this.isFixed = false;
+                }
+                this.isFixed = false;
+            },
+            beforeThreshold(v) {
+                this.$refs.bookmark.style.top = `${-v}px`;
+                this.beforeHeight();
+                const iconDown = this.$refs.iconDown;
+                if (iconDown.style.transform === 'rotate(180deg)') {
+                    iconDown.style.transform = 'rotate(0deg)';
+                }
+            },
+            afterThreshold(v) {
+                //超越临界状态
+                this.$refs.bookmark.style.top = `${-v}px`;
+                if (this.isBookmark) {
+                    this.text = this.$t('book.releaseDeleteMark');
+                    this.color = BLUE;
+                    this.isFixed = false;
+                } else {
+                    this.text = this.$t('book.releaseAddMark');
+                    this.color = BLUE;
+                    this.isFixed = true;
+                }
+                const iconDown = this.$refs.iconDown;
+                if (iconDown.style.transform === '' || iconDown.style.transform === 'rotate(0deg)') {
+                    iconDown.style.transform = 'rotate(180deg)';
+                }
+                this.isFixed = true;
+
             }
         }
     }
